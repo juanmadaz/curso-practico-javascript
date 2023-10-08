ddf5485f4c3cb2b3d9202e448f66669db3a4c8cf
Juan Dávila <juanmdavilazamora@outlook.com>
2023-10-08 15:51:10 -0600

    modificacion de dom basica

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const pClass = document.querySelector('.pClass');
+const pId = document.querySelector('#pId');
+const input = document.querySelector('input');
+
+console.log(input.value);
+
+console.log({
+    h1,
+    p,
+    pClass,
+    pId,
+    input
+});
