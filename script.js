e655687ee3756cdbc4f20525b85c3047a0088ee0
Juan Dávila <juanmdavilazamora@outlook.com>
2023-10-17 16:04:25 -0600

    eventos en javascript

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const pClass = document.querySelector('pClass');
-const pId = document.querySelector('#pId');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular')
+const pResult = document.querySelector('#result')
 
-// console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    pClass,
-    pId,
-    input
-});
-
-h1.innerText = 'Cambio desde <br> JS'
-// h1.innerHTML = 'Cambio desde <br> JS'
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo')
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-// h1.classList.toogle('verde');
-// h1.classList.contains('verde');
-
-input.value='7777777777';
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80')
-console.log(img);
-pId.innerText = '';
-pId.appendChild(img);
-console.log({pId});
+function btnOnClick() {
+    const sumaInputs = Number(input1.value) + Number(input2.value); // unary plus (+input1.value)
+    pResult.innerText = "Resultado: " +sumaInputs;
+}
