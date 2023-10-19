31c230c1a479cd90c77da9aa8c83b352029c3afa
Juan Dávila <juanmdavilazamora@outlook.com>
2023-10-19 14:50:42 -0600

    add vent listener

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular')
 const pResult = document.querySelector('#result')
 
-function btnOnClick() {
+form.addEventListener('submit', sumaInputs)
+
+function sumaInputs(event) {
+    console.log(event);
+    event.preventDefault();
     const sumaInputs = Number(input1.value) + Number(input2.value); // unary plus (+input1.value)
     pResult.innerText = "Resultado: " +sumaInputs;
 }
